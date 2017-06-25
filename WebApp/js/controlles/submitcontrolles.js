8cca5822984de3a58968c2bf4a67bc3391ab89e1
salmon <dongwenbingxc@qq.com>
2017-06-25 15:34:20 +0800

    业务表单案例

---
+++ WebApp/js/controlles/submitcontrolles.js
@@ -0,0 +1,32 @@
+  var vm = new Vue({
+      el: '#selectCity',
+      data: {
+          provincedefual: "请选择...",
+          citydefual: "请选择...",
+          area: "请选择...",
+          items: null,
+          province: null,
+          city: null,
+          area: null,
+          scotop: 0
+      },
+      methods: {
+          getdata: function() {
+              this.$http.get("/dev/data/nv/province.json").then(function(rtdata) {
+                      if (!rtdata) return false;
+                      rtdata = eval("(" + rtdata.body + ")");
+                      this.items = rtdata;
+                  },
+                  function(error) {})
+          },
+
+          pickscorll: function(event) {
+              this.scotop = event.target.scrollTop;
+              var itemheight = event.target.scrollHeight / (this.items.length + 2)
+
+              //   alert("" + this.items[6].ProID + "");
+          }
+      },
+  })
+
+  vm.getdata();
---
+++ WebApp/js/controlles/userinfocontrolles.js
@@ -0,0 +1,22 @@
+﻿var userinfo = {}
+
+userinfo.app = new Vue({
+    el: '#app',
+    data: {
+        message: '微信js接口实例页!'
+    },
+    methods: {
+        getdata: function () {
+            this.$http.get("/Weixin/WeixinSdk").then(function (rtdata) {
+                if (!rtdata) return false;
+                rtdata = eval("(" + rtdata.body + ")");
+                weixinapi.load(rtdata);
+                return rtdata;
+            },
+                function (error) { })
+        }
+    }
+})
+
+//userinfo.app.getdata();
+
